103b1907c5e14a88cdddd9408031563b1ce260b9
İbrahim <ibrahim.karadag@yemekpos.com>
2024-06-19 22:48:56 +0300

    Update BookModel.cs

--- Entity/BookModel.cs
+++ Entity/BookModel.cs
@@ -2,5 +2,16 @@
 {
     public class BookModel
     {
+        
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Writer { get; set; }
+        public DateTime YearOfPublication { get; set; }
+        public string Category { get; set; }
+        public int Count { get; set; }
+
+
     }
 }
